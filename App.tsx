0c72aade15a1740689c3e2f44011fb27f0ec3e04
徐鑫 <ext_xuxin03@kuaishou.com>
2026-04-13 17:24:51 +0800

    重构App和Cat组件，以增强功能和用户体验。引入了一个表格，用于添加带有名字、年龄和最喜欢的食物的新猫。更新了Cat组件，以顯示最喜歡的食物，並改進了造型，以提高可讀性。根据主题调整背景颜色。

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
 import { useState } from 'react';
-import { Button, StatusBar, StyleSheet, Text, TextInput, useColorScheme, View } from 'react-native';
+import {
+  Button,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -7,7 +15,6 @@ import {
 import Cat from './Cat';
 
 function App() {
-
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
@@ -24,70 +31,181 @@ function AppContent() {
   const isDarkMode = useColorScheme() === 'dark';
 
   const themeColors = {
-    background: isDarkMode ? '#101214' : '#ffffff',
+    background: isDarkMode ? '#101214' : '#fdb',
     text: isDarkMode ? '#f5f5f5' : '#111111',
     inputBackground: isDarkMode ? '#1b1d21' : '#ffffff',
     inputBorder: isDarkMode ? '#3a3f47' : '#6fc',
     placeholder: isDarkMode ? '#9aa0aa' : '#666666',
   };
 
-  const [inputA, setInputA] = useState('')
+  const [catForm, setCatForm] = useState({
+    name: '',
+    age: '',
+    favoriteFood: '',
+  });
 
-  const cats = [
-    { name: 'Tom', age: 12 },
-    { name: 'Bob', age: 16 },
-  ];
+  const [cats, setCats] = useState([
+    {
+      name: 'Tom',
+      age: 12,
+      favoriteFood: '小鱼干',
+    },
+    {
+      name: 'Bob',
+      age: 16,
+      favoriteFood: '猫薄荷',
+    },
+  ]);
 
+  const handleAddCat = () => {
+    if (
+      catForm.name === '' ||
+      catForm.favoriteFood === '' ||
+      catForm.age === ''
+    ) {
+      return;
+    }
+
+    const catAgeNumber = Number(catForm.age);
+
+    if (Number.isNaN(catAgeNumber) || catAgeNumber < 0) {
+      return;
+    }
+
+    setCats([
+      ...cats,
+      {
+        name: catForm.name,
+        age: catAgeNumber,
+        favoriteFood: catForm.favoriteFood,
+      },
+    ]);
+    setCatForm({
+      name: '',
+      age: '',
+      favoriteFood: '',
+    });
+  };
+
+  const handleChangeCatForm = (
+    key: 'name' | 'age' | 'favoriteFood',
+    value: string,
+  ) => {
+    setCatForm({
+      ...catForm,
+      [key]: value,
+    });
+  };
 
   return (
     <View
       style={[
         styles.container,
         {
-          paddingTop: safeAreaInsets.top, paddingBottom: safeAreaInsets.bottom,
-          backgroundColor: themeColors.background
+          paddingTop: safeAreaInsets.top,
+          paddingBottom: safeAreaInsets.bottom,
+          backgroundColor: themeColors.background,
         },
       ]}
     >
+      <Button title="添加猫🐱" onPress={handleAddCat}></Button>
 
-      {cats.map((cat) => (
+      {cats.map(cat => (
         <Cat
+          textColor={themeColors.text}
+          cardBackground={themeColors.inputBackground}
           key={cat.name}
-          name={cat.name} age={cat.age} textColor={themeColors.text} cardBackground={themeColors.inputBackground} />))}
-      <Text
-        style={{
-          color: themeColors.text,
-          textAlignVertical: 'center', alignSelf: 'center', minHeight: 40
-        }}
-      >
-        {inputA || '先输入一些文本'}
-      </Text>
-      <TextInput
-        style={[styles.input, {
-          color: themeColors.text,
-          borderColor: themeColors.inputBorder,
-          backgroundColor: themeColors.inputBackground
-        }]}
-        placeholderTextColor={themeColors.placeholder}
-        value={inputA}
-        onChangeText={setInputA}
-        placeholder='在这里输入内容'
-      />
+          name={cat.name}
+          age={cat.age}
+          favoriteFood={cat.favoriteFood}
+        />
+      ))}
+      <View>
+        <View>
+          <Text style={[styles.textShow, { color: themeColors.text }]}>
+            {catForm.name ? '猫的名字是：' + catForm.name : '输入名字'}
+          </Text>
+          <TextInput
+            style={[
+              styles.input,
+              {
+                color: themeColors.text,
+                borderColor: themeColors.inputBorder,
+                backgroundColor: themeColors.inputBackground,
+              },
+            ]}
+            placeholderTextColor={themeColors.placeholder}
+            value={catForm.name}
+            onChangeText={text => handleChangeCatForm('name', text)}
+            placeholder="在这里输入名字"
+          />
+        </View>
+        <View>
+          <Text style={[styles.textShow, { color: themeColors.text }]}>
+            {catForm.age ? '猫的年龄是：' + catForm.age : '输入年龄'}
+          </Text>
+          <TextInput
+            keyboardType="numeric"
+            style={[
+              styles.input,
+              {
+                color: themeColors.text,
+                borderColor: themeColors.inputBorder,
+                backgroundColor: themeColors.inputBackground,
+              },
+            ]}
+            placeholderTextColor={themeColors.placeholder}
+            value={catForm.age}
+            onChangeText={text => handleChangeCatForm('age', text)}
+            placeholder="在这里输入年龄"
+          />
+        </View>
+        <View>
+          <Text style={[styles.textShow, { color: themeColors.text }]}>
+            {catForm.favoriteFood
+              ? '猫的爱好是：' + catForm.favoriteFood
+              : '输入爱好'}
+          </Text>
+          <TextInput
+            style={[
+              styles.input,
+              {
+                color: themeColors.text,
+                borderColor: themeColors.inputBorder,
+                backgroundColor: themeColors.inputBackground,
+              },
+            ]}
+            placeholderTextColor={themeColors.placeholder}
+            value={catForm.favoriteFood}
+            onChangeText={text => handleChangeCatForm('favoriteFood', text)}
+            placeholder="在这里输入爱好"
+          />
+        </View>
+      </View>
       <Button
-        title='清空'
-        onPress={() => { setInputA('') }}
+        title="清空"
+        onPress={() => {
+          setCatForm({
+            name: '',
+            age: '',
+            favoriteFood: '',
+          });
+        }}
       />
     </View>
   );
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 16,
-    gap: 12, 
+    gap: 12,
+  },
+  textShow: {
+    textAlignVertical: 'center',
+    alignSelf: 'center',
+    minHeight: 40,
   },
   input: {
     borderWidth: 1,
@@ -95,9 +213,9 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     paddingVertical: 6,
     paddingHorizontal: 10,
-    minHeight: 70,
-    textAlignVertical: 'top'
-  }
+    minHeight: 50,
+    textAlignVertical: 'top',
+  },
 });
 
 export default App;
--- Cat.tsx
+++ Cat.tsx
@@ -2,49 +2,59 @@ import React, { useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 type CatProps = {
-    name: string
-    age: number;
-    textColor: string;
-    cardBackground: string;
-}
+  textColor: string;
+  cardBackground: string;
+  name: string;
+  age: number;
+  favoriteFood: string;
+};
 
-const Cat = ({ name, age, textColor, cardBackground }: CatProps) => {
-    const [isHungry, setIsHungry] = useState(true)
-    const textStyle = { color: textColor }
+const Cat = ({
+  name,
+  age,
+  textColor,
+  cardBackground,
+  favoriteFood,
+}: CatProps) => {
+  const [isHungry, setIsHungry] = useState(true);
+  const textStyle = { color: textColor };
 
-    const handleToggleHungry = () => { setIsHungry(!isHungry) }
+  const handleToggleHungry = () => {
+    setIsHungry(!isHungry);
+  };
 
-    return (
-        <View
-            style={[styles.card, {
-                backgroundColor: cardBackground,
-            }]}
-        >
-            <Text
-                style={[styles.text, textStyle]}
-            >我是CatRN组件</Text>
-            <Text
-                style={[styles.text, textStyle]}>名字是：{name}，年龄是{age}  </Text>
-            <Text
-                style={[styles.text, textStyle]}>{isHungry ? '我饿了' : '我吃饱了'}  </Text>
-            <Button
-                title='吃饭'
-                onPress={handleToggleHungry}
-            />
-        </View>
-    );
+  const hungryText = isHungry ? '我饿了' : '我吃饱了';
+  return (
+    <View
+      style={[
+        styles.card,
+        {
+          backgroundColor: cardBackground,
+        },
+      ]}
+    >
+      <Text style={[styles.text, textStyle]}>我是CatRN组件</Text>
+      <Text style={[styles.text, textStyle]}>
+        名字是：{name}，年龄是{age}{' '}
+      </Text>
+      <Text style={[styles.text, textStyle]}>最爱的是：{favoriteFood} </Text>
+      <Text style={[styles.text, textStyle]}>{hungryText} </Text>
+      {isHungry && <Text style={[styles.text, textStyle]}>让我吃些零食 </Text>}
+      <Button title="吃饭" onPress={handleToggleHungry} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    text: {
-
-    },
-    card: {
-        paddingHorizontal: 10,
-        borderRadius: 16,
-        paddingVertical: 12,
-        gap: 6
-    }
-})
+  text: {
+    fontSize: 14,
+    lineHeight: 16,
+  },
+  card: {
+    borderRadius: 16,
+    padding: 20,
+    gap: 6,
+  },
+});
 
 export default Cat;
